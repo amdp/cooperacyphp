97cfb4fd18f2a0879935119ee8c28515b08abd32
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-02-19 01:02:14 +0100

    howitworks reedit

--- resources/views/pages/platform.blade.php
+++ resources/views/pages/platform.blade.php
@@ -37,13 +37,13 @@
   <p class="base">When an idea is funded and produces wealth, its profits go back to the platform and get proportionally redistributed in order to fund other ideas, creating a virtuous circle that generates wealth for everyone.</p>
 
   <span class="subheading equivalence">Ecosystemic Learning</span>
-  <p class="base">An artificial intelligence will analyse all the data coming from the platform and learn, in the long run, best practices in order to help the people to realise their ideas. We think about this as an ecosystemic deep learning. The platform will in facts allow us to choose the best projects and proportionally finance them according to the principles of cooperation.</p>
+  <p class="base">An artificial intelligence will analyse all the data coming from the platform and learn, in the long run, best practices in order to help the people to realise their ideas. We think about this as an ecosystemic deep learning. The platform will in facts allow us to choose the best projects and proportionally finance them according to the principles of cooperation. This could seem a big controlling machine system, but there's a catch: participants can communicate to the Cooperacy ecosystem their happiness, so that the Ecosystemic Learning will base its choices also on the most human, irrational and personal information of all: mood.</p>
 
   <p class="subheading transparency center space margin">IMAGINE</p>
   <p class="base">Imagine the effect of one million people bringing together their little economic effort. Imagine a million euros per month transforming the world. This money flow will be used only when a common will is found. Only jointly enjoyable ideas will be realized. Imagine a social change that is possible exclusively according to the number of people participating.<br>
   Would you like to help this to happen?</p>
 
-  <p class="base"><i>For more technical information about the platform, see our <a href="/howitworks">white paper.<a></i></p>
+  <p class="base"><i>For more technical information see the platform <a href="/howitworks">white paper.<a></i></p>
 
   </div>
   <!--END CONTENT-->
