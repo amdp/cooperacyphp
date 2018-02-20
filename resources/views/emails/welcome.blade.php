e2eb38dafb3791c0e568a5b2758784cd5c5b9e85
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-02-20 12:14:50 +0100

    mail edit

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,15 +1,3 @@
-<p>Ciao, siamo molto contenti che tu sia interessato a Cooperacy.<br>
-Ci teniamo ad informarti che per completare la tua iscrizione alla nostra associazione, è necessario finalizzare il pagamento che corrisponde ad 1&#8364; al mese.</p>
-
-<p>Clicca su questo link <a href="https://www.cooperacy.org/login">www.cooperacy.org/login</a> per completare il tutto.</p>
-
-<p>Ad oggi le nostre iscrizioni possono essere effettuate tramite PayPal, e se dovessi avere delle difficoltà scrivici a questo indirizzo cooperacy@cooperacy.org, faremo il possibile per aiutarti.</p>
-
-<p>Speriamo di averti presto tra di noi.</p>
-
-<p>Il team Cooperacy</p>
-
-<hr>
 
 <p>Hi, we are delighted you are interested in Cooperacy.<br>
 We would like to inform you that in order to complete your subscription to our association, you should finalize the payment which corresponds to 1&#8364; per month.</p>
@@ -18,8 +6,20 @@ We would like to inform you that in order to complete your subscription to our a
 
 <p>Currently our subscriptions run through PayPal, and in case of trouble you can get in contact with cooperacy@cooperacy.org, we'll do our best to help you.</p>
 
-<p>We hope you will join us very soon!</p>
+<p>We hope you will join us soon!</p>
 
 <p>Cooperacy Team</p>
 
 
+<hr>
+
+<p>Ciao, siamo molto contenti che tu sia interessato a Cooperacy.<br>
+Ci teniamo ad informarti che per completare la tua iscrizione alla nostra associazione, è necessario finalizzare il pagamento che corrisponde ad 1&#8364; al mese.</p>
+
+<p>Clicca su questo link <a href="https://www.cooperacy.org/login">www.cooperacy.org/login</a> per completare il tutto.</p>
+
+<p>Ad oggi le nostre iscrizioni possono essere effettuate tramite PayPal, e se dovessi avere delle difficoltà scrivici a questo indirizzo cooperacy@cooperacy.org, faremo il possibile per aiutarti.</p>
+
+<p>Speriamo di averti presto tra di noi!</p>
+
+<p>Il team Cooperacy</p>
