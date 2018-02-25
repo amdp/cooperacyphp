48578f4469960cee481819b77993980ae0ec5249
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-02-25 21:57:14 +0100

    little edit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!--BEGIN FOOTER-->
 <div id="footer" class="site-footer">
 <!-- #copyright -->
-          <p class="base center space" style="margin-bottom:2px"><a href="{{route('register')}}" class="aue" style="background-image:none">JOIN</a> <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YS7MHXBSVDDEU" class="aut" style="background-image:none">DONATE</a></p>
+          <p class="base center space" style="margin-bottom:2px"><a href="{{route('register')}}" class="aue" style="background-image:none">JOIN</a> OR <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YS7MHXBSVDDEU" class="aut" style="background-image:none">DONATE</a></p>
           <p class="mini center space">
           &copy; COOPERACY RESEARCH ASSOCIATION 2015-2018 - VIA CAPPUCCINI, 11 - 20122 MILAN - FISCAL CODE 97725540153<br><a class="auh" href="https://www.iubenda.com/privacy-policy/8141188">PRIVACY POLICY</a> - <a class="auh" href="{{route('terms')}}">AGREEMENTS</a> - <a class="auh" href="/contact">CONTACT</a></p>
 </div>
