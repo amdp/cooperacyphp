83cc0b5be168eb06d9a4b025b4ba64237fde4a1e
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-03-01 16:23:05 +0100

    proportions > integration

--- resources/views/pages/cooperation.blade.php
+++ resources/views/pages/cooperation.blade.php
@@ -27,18 +27,18 @@
   According to our scientific model, seven conditions should be met.
   </p>
 
-  <br><img class="page-header" src="{{asset('/images/diversity.png')}}"><br>
+  <br><img id="diversity" name="diversity" class="page-header" src="{{asset('/images/diversity.png')}}"><br>
 
     <p class="subheading diversity center space margin">WE'RE DIFFERENT</p>
-    <p class="base">Diversity is a pre-condition of cooperation that always exists in any ecosystem. It is a condition because it <i>should not be inhibited</i>. Cooperation is different from fanaticism and homologation, where every individual must conform to a unique idea, lifestyle, cult, imagery or human mechanisation. Standardised jobs or constrained life dynamics, like workers in the industrial revolution factories, life in feudal times or peasants under large landowners control, inhibit the emergence of cooperation.</p>
+    <p class="base">Diversity is a pre-condition of cooperation that always exists in any ecosystem. It is a condition because it <i>should not be inhibited</i>. Cooperation is different from fanaticism and homologation, where every individual must conform to a unique idea, lifestyle, cult, imagery or human mechanisation. Without diversity, we lose beauty and innovation. Standardised jobs or constrained life dynamics, like workers in the industrial revolution factories, inhibit the emergence of cooperation.</p>
 
     <p class="subheading diversity center space margin">OUR EVOLUTIVE ADVANTAGE</p>
-    <p class="base">Anthropologically mankind reached high levels of survival and wealth also thanks to the natural tendency to pursue and develop different personal skills, generating the possibility to have more roles and functions for every participant of the community. Probably we are the most diverse and role-differentiated creatures on earth.</p>
+    <p class="base">Diversity is a potential of conflict, but also a potential of development. Anthropologically mankind reached high levels of survival and wealth also thanks to the natural tendency to pursue and develop different personal skills, generating the possibility to have more roles and functions for every participant in the community. Probably we are the most diverse and role-differentiated creatures on earth.</p>
 
     <p class="subheading diversity center space margin">PERSONALITIES</p>
-    <p class="base">The acknowledgement of different personalities and roles in the community is needed to create a good climate: when individuals have their personal identity respected by the group they accomplish great common results leading to community-wide advantages.</p>
+    <p class="base">The acknowledgement of different preferences, personalities and roles in the community is needed to create a good climate: when individuals have their personal identity respected by the group they accomplish great common results leading to community-wide advantages.</p>
 
-  <br><img class="page-header" src="{{asset('/images/understanding.png')}}"><br>
+  <br><img id="understanding" name="understanding" class="page-header" src="{{asset('/images/understanding.png')}}"><br>
 
     <p class="subheading understanding center space margin">DIFFERENT PERSPECTIVES</p>
     <p class="base">When diversity brings different linguistic, behavioural, and cultural codes, we need the presence of common background that can act as a bridge to allow cooperation to emergence.</p>
@@ -49,7 +49,7 @@
     <p class="subheading understanding center space margin">COMMON CODES</p>
     <p class="base">Integrating and explaining languages and cultures, personalities and habits, points of view and interpretations brings more understanding. Learning the beauty of a different way to interpret the reality and enjoying the pleasure of the difference is a great cooperation drive. While religions, traditions and languages may be different, values like love, respect, safety or concepts like friendship, birth and parenthood can be strong common codes upon which we can build cross-diversity understanding. Enhancing empathy, the special common code of feelings, we can generate deep comprehension, leading everyone feel accepted and part of the community. All these concepts help cooperation to emerge together with mutual enjoyment.</p>
 
-  <br><img class="page-header" src="{{asset('/images/freedom.png')}}"><br>
+  <br><img id="freedom" name="freedom" class="page-header" src="{{asset('/images/freedom.png')}}"><br>
 
     <p class="subheading freedom center space margin">CHOICES</p>
     <p class="base">We can say there’s a generic collaboration even in slavery, imposition or manipulation, as people effectively do something together. They're not that happy, though. Lack of personal freedom, and absence of a personal choice in the collaborative interaction make it impossible to make it become a cooperation: there is no mutual enjoyment at all.</p>
@@ -61,7 +61,7 @@
     <p class="base">One of the most important expressions of freedom isn’t the possibility to do anything that comes up in our mind, but an adequate, balanced social context that allows personal space and access to services for everyone.</br>
     Once again, it is in the balance of the whole social ecosystem the secret that allows individual, intimate, personal freedom.</p>
 
-  <br><img class="page-header" src="{{asset('/images/transparency.png')}}"><br>
+  <br><img id="transparency" name="transparency" class="page-header" src="{{asset('/images/transparency.png')}}"><br>
 
     <p class="subheading transparency center space margin">TRUTHS</p>
     <p class="base">Honesty is regarded as one of the most important values in many cultural contexts. Nevertheless, truth is difficult to say and difficult to receive: it should be communicated with the right words and the right attitude. Sometimes, instead of communicating the truth, it is possible to communicate the <i>point of view</i> with which we see things now or made a choice before.</p>
@@ -70,10 +70,10 @@
     <p class="subheading transparency center space margin">FORGIVENESS</p>
     <p class="base">Do we want to know the truth to understand and take account of it or just to punish others if they made a mistake? Behind the exchange of truth there’s a great deal of acceptance. Most of us don’t say what they think just because they are afraid of the consequences. Only accepting the diversity of others we can know the reality behind the appearances. Being honest and transparent seems hard, but it is one of the ways in which you can change the world and your same personal life. The right social ecosystem supports transparency and truth acceptance.</p>
 
-  <br><img class="page-header" src="{{asset('/images/care.png')}}"><br>
+  <br><img id="care" name="care" class="page-header" src="{{asset('/images/care.png')}}"><br>
 
     <p class="subheading care center space margin">COORDINATION</p>
-    <p class="base">The condition of "care" is related to coordination and respect of the ecosystems. When we receive services, like when we take a bus or a taxi, when our nations give us healthcare or educational facilities, when our families fulfill some of our needs or desires, we receive care. That is coordinating something needed by you with something needed by others.</p>
+    <p class="base">The condition of "care" is related to coordination and respect of the social and natural ecosystems. When we receive services, like when we take a bus or a taxi, when our nations give us healthcare or educational facilities, when our families fulfill some of our needs or desires, when we hug each other, we receive care. Care means coordinating something needed by you with something needed by others.</p>
     <p class="subheading care center space margin">EVERYONE AROUND YOU</p>
     <p class="base">Caring for others is a form of complex thinking that is able to generate mutual enjoyment. Usually care is not perceived as something fun or productive, but everyone of us needs and likes to receive attention. When respect and esteem are shared amongst everyone, productivity levels raise and even fun is better.</p>
     <p class="subheading care center space margin">ECOSYSTEMS</p>
@@ -81,7 +81,7 @@
     Unhealthy workplaces and local or global damaged environment, psychological dependencies, absence of meritocracy, mobbing and in the worst cases violence can’t lead to the emergence of cooperation.</br>
     The natural ecosystem is the base for our social context to be pleasant. Humans, animals, plants, mountains, deserts and oceans: the whole planet is connected and every part of it is needed like the organs of your body. The whole ecosystem is a living being.</p>
 
-  <br><img class="page-header" src="{{asset('/images/trust.png')}}"><br>
+  <br><img id="trust" name="trust" class="page-header" src="{{asset('/images/trust.png')}}"><br>
 
     <p class="subheading trust center space margin">WHAT IS TRUST?</p>
     <p class="base">When we are not respected or cared, we become mistrustful, because we developed a powerful evolutionary tool: trust. in neuroscience, trust is represented by the probability, based on similar or past events, that an interaction will be pleasurable in its results, called rewards. Rewards can be intrinsic, like something you personally like to do, or extrinsic, like when you like money just because <i>then</i> using money you will do something you like. When your rewards are mainly linear and extrinsic, that is, external to your self identity, you start to lose your native personality. You may even build up two or more contrasting selves, one who wants to live in the nature with few things and another one who wants to be rich and live in a skyscraper.<br>
@@ -93,19 +93,19 @@
     <p class="subheading trust center space margin">BETRAYAL AND FEAR</p>
     <p class="base">When we are betrayed we tend to feel anger and a desire of damaging the person we thought was worth of trust, because the opposite of trust is fear, and fear brings anger.</br> Social systems can be punitive, like dictatorship and totalitarianism, or  overly based on benefits and performance, like extreme competitive money-making based societies. In that case the high risks generate anger, damage intrinsic trust in people and individuals seek extrinsic trust and rewards. We look for friends who can give us benefits, not happiness, because without benefits we may suffer, be afraid or feel excluded from the social network. Trust is very important, because when people are afraid or mistrust others, conflicts may arise quite often. It is easy to understand how trust depends on the whole social ecosystem balance and how important it is for the emergence of cooperation and enjoyment.</p>
 
-  <br><img class="page-header" src="{{asset('/images/equivalence.png')}}"><br>
+  <br><img id="equivalence" name="equivalences" class="page-header" src="{{asset('/images/equivalence.png')}}"><br>
 
-  <p class="subheading equivalence center space margin">EQUALITY?</p>
-  <p class="base">Equivalence is the simplest condition and evidence of cooperation. Everyone of us is different, but the idea that everyone is happy reminds us of beautiful moments of sharing where our happiness was even stronger because it was also in the eyes of the others around us. The idea of being "equal" seems right, but in reality it is not. If I am a little kid, I probably need a different meal compared to a woman or a man.<br />
-  A kid meal, even if of less amount, is <i>equivalent</i> to a standard meal for a grown-up human. Kids may also prefer more sweets than adults, but shouldn't like a glass of wine. Diabetics may want the same sweets as their kids, but alas, they know it wouldn't be a good choice. Individuals and their diversity should be considered for equivalent benefits, not equal ones, in order to live <i>equivalent</i> lives.</p>
+    <p class="subheading equivalence center space margin">EQUALITY?</p>
+    <p class="base">Equivalence is the simplest condition and evidence of cooperation. Everyone of us is different, but the idea that everyone is happy reminds us of beautiful moments of sharing where our happiness was even stronger because it was also in the eyes of the others around us. The idea of being "equal" seems right, but in reality it is not. If I am a little kid, I probably need a different meal compared to a woman or a man.<br />
+    A kid meal, even if of less amount, is <i>equivalent</i> to a standard meal for a grown-up human. Kids may also prefer more sweets than adults, but shouldn't like a glass of wine. Diabetics may want the same sweets as their kids, but alas, they know it wouldn't be a good choice. Individuals and their diversity should be considered for equivalent benefits, not equal ones, in order to live <i>equivalent</i> lives.</p>
 
-  <p class="subheading equivalence center space margin">COOPERATION IS DIFFERENT THAN ALTRUISM</p>
-  <p class="base">Cooperation is different from altruism, solidarity and donation where we see a “recipient” that simply “gets” in a one-way action. It is easy to say that if you follow only your personal benefit, you are in the world of egoism. Sometimes, instead, you are only focused on giving and you forget about yourself. Cooperation is an equilibrium of the two, a situation in which you're selfish while being altruist -and feel happy about it.<br/>
-  Cooperation is complex: it is way more than the simple idea of giving or getting and emerges when the two actions find their equivalence.</p>
+    <p class="subheading equivalence center space margin">COOPERATION IS DIFFERENT THAN ALTRUISM</p>
+    <p class="base">Cooperation is different from altruism, solidarity and donation where we see a “recipient” that simply “gets” in a one-way action. It is easy to say that if you follow only your personal benefit, you are in the world of egoism. Sometimes, instead, you are only focused on giving and you forget about yourself. Cooperation is an equilibrium of the two, a situation in which you're selfish while being altruist -and feel happy about it.<br/>
+    Cooperation is complex: it is way more than the simple idea of giving or getting and emerges when the two actions find their equivalence.</p>
 
-  <p class="subheading equivalence center space margin">BENEFITS AND RELATIONS</p>
-  <p class="base">Equivalence is not a mere accounting of things. You may enjoy food, a new car, but also a good friendship. An equivalence of real benefits is found when anybody gets equivalent goods, services or valuables as the other participants. An equivalence of relational benefits, instead, happens when you are as averagely happy as the other participants are. The first one is good, it is usually called a win-win, but it is not enough. Cooperation is not a mere goods and services exchange, but a pleasant relational exchange too.
-  Cooperation emerges -and it found as evident- when in a social ecosystem there are good benefits and good relations for everyone.<br><br><br></p>
+    <p class="subheading equivalence center space margin">BENEFITS AND RELATIONS</p>
+    <p class="base">Equivalence is not a mere accounting of things. You may enjoy food, a new car, but also a good friendship. An equivalence of real benefits is found when anybody gets equivalent goods, services or valuables as the other participants. An equivalence of relational benefits, instead, happens when you are as averagely happy as the other participants are. The first one is good, it is usually called a win-win, but it is not enough. Cooperation is not a mere goods and services exchange, but a pleasant relational exchange too.
+    Cooperation emerges -and it found as evident- when in a social ecosystem there are good benefits and good relations for everyone.<br><br><br></p>
 
   <p class="base">To support your social context to become a social ecosystem, learn about the <a class="aue" href="/integration">proportional creative integration</a> and the different <a class="aue" href="/solutions">solutions</a> that can resolve eventual contextual contrasts.</p>
 
