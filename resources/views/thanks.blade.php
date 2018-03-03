467f7baa3886f2035d9bdd31e606881e771798ac
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-03-03 15:32:19 +0100

    cooperation page links

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
         <div class="jumbotron">
-            <h1>THANK YOU</h1>
-            <h2>YOU ARE NOW A MEMBER OF COOPERACY.</h2>
-            <a href="{{url('/home')}}"><h2>GO TO DASHBOARD</h2></a>
+            <h1>THANK YOU!</h1>
+            <h2>CONGRATULATIONS, YOU ARE NOW A MEMBER OF COOPERACY.</h2>
+            <a href="{{url('/home')}}"><h2>GO TO BETA DASHBOARD</h2></a>
         </div>
     </div>
 </div>
