f61ab382b9cf50984280a81fb8ee0723a2edeb3d
Enea Barbetta <enea.barbetta@gmail.com>
2018-04-15 16:57:30 +0200

    (Admin) Update on PayPal transactions, memberships, roles

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -76,9 +76,13 @@ class PaymentController extends Controller
         $email = Auth::user()->email;
 
         //Create transaction
+        //Sandbox or Live?
+        $type = DB::table('site_options')->where('option_name','PayPalApi')->value('option_value');
+
         DB::table('paypal_transactions')->insert([
             'user_id'       =>  Auth::user()->id,
             'user_email'    =>  $email,
+            'type'          =>  $type,
             'plan_id'       =>  $plan->plan_id,
             'charge_id'     =>  $plan->charge_id
         ]);
@@ -90,23 +94,29 @@ class PaymentController extends Controller
     }
 
     public function confirm(Request $request) {
-        
+
+        //Sandbox or Live?
+        $type = DB::table('site_options')->where('option_name','PayPalApi')->value('option_value');
+
         if(!empty($request->token)) {
         DB::table('paypal_transactions')
             ->where('user_id', Auth::user()->id)
-            ->where('user_email',Auth::user()->email)
-            ->where('status','uncomplete')
+            ->where('user_email', Auth::user()->email)
+            ->where('type', $type)
+            ->where('status', 'uncomplete')
             ->update(['token'=>$request->token]);
             $transaction = DB::table('paypal_transactions')
             ->where('user_id', Auth::user()->id)
-            ->where('user_email',Auth::user()->email)
-            ->where('status','uncomplete')
+            ->where('user_email', Auth::user()->email)
+            ->where('type', $type)
+            ->where('status', 'uncomplete')
             ->first();
         } else {
         $transaction = DB::table('paypal_transactions')
             ->where('user_id', Auth::user()->id)
-            ->where('user_email',Auth::user()->email)
-            ->where('status','uncomplete')
+            ->where('user_email', Auth::user()->email)
+            ->where('type', $type)
+            ->where('status', 'uncomplete')
             ->first();
         }
         
@@ -116,9 +126,43 @@ class PaymentController extends Controller
 
     public function pay(Request $request) {
 
-        $executepayment = new PayPalFunctions;
+        $pay = new PayPalFunctions;
+        $agreement = $pay->pay($request->token);
 
-        return $executepayment->pay($request->token);
+        //Sandbox or Live?
+        $type = DB::table('site_options')->where('option_name','PayPalApi')->value('option_value');
+
+        //Update transaction table
+        DB::table('paypal_transactions')
+            ->where('user_id', Auth::user()->id)
+            ->where('user_email', Auth::user()->email)
+            ->where('type', $type)
+            ->where('status', 'uncomplete')
+            ->whereNotNull('token')
+            ->update([
+                'status'        => 'complete',
+                'agreement_id'  => $agreement->id
+            ]);
+        
+        //Create subscription
+        DB::table('paypal_memberships')->insert([
+            'user_id'       =>  Auth::user()->id,
+            'agreement_id'  =>  $agreement->id,
+            'state'         =>  $agreement->state,
+            'type'          =>  $type,
+            'paypal_email'  =>  $agreement->payer->payer_info->email
+        ]);
+
+        //Update member status
+        DB::table('users')->where('id', Auth::user()->id)->update([
+            'member_role'       =>  'user',
+            'member_type'       =>  'member',
+            'member_payment'    =>  'PayPal',
+            'member_status'     =>  $agreement->state,
+        ]);
+        
+            //return $agreement;
+        return view('auth.thanks');
 
     }
 
---
+++ resources/views/auth/thanks.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+        <div class="jumbotron">
+            <h1>THANK YOU!</h1>
+            <h2>CONGRATULATIONS, YOU ARE NOW A MEMBER OF COOPERACY.</h2>
+            <a href="{{url('/home')}}"><h2>GO TO BETA DASHBOARD</h2></a>
+        </div>
+    </div>
+</div>
+@endsection
