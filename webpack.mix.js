49d72eb32965cf1dc5bc173cfb553605971c2da7
Marcos Siqueira Junior <junior.crvg@gmail.com>
2018-06-16 17:13:03 -0300

    Moves testimonials to a slideshow

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,9 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.autoload({
+    jQuery: 'jquery',
+    $: 'jquery',
+    jquery: 'jquery'
+});
