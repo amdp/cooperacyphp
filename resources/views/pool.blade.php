51e536c108586d55b3fe956ea811f005f7a2cd8c
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-07-16 09:54:15 +0200

    contact form in pool page

--- resources/views/pool.blade.php
+++ resources/views/pool.blade.php
@@ -16,9 +16,44 @@
     If you are living in a low income country, you can pool with a lower amount if you help us adapt the amount to the different purchasing power of your nation.<br><br>
 
     {!! Form::open(array('url' => 'pool', 'method' => 'post')) !!}
-                    {!! Form::submit('Voluntary pooling through PayPal', ['class'=>'btn btn-primary btn-block']) !!}
+                    {!! Form::submit('VOLUNTARY POOLING THROUGH PAYPAL', ['class'=>'btn btn-primary btn-block']) !!}
                     {!! Form::close() !!}
-    <br><a class="ae" href="mailto:membershp@cooperacy.org"><div class="btn btn-primary btn-block">Send us your voluntary pooling bank transfer receipt instead</div></a></p>
+    <br><br>
+
+    <p class="base">Pool instead via bank transfer sending us a code/receipt or contact us for suggestions, questions, requests:</p>
+    <div role="form" class="wpcf7" id="wpcf7-f1303-p20-o1" lang="en-US" dir="ltr">
+    {{ Form::open(array('url' => 'sendmessage', 'novalidate'=>'novalidate')) }}
+
+    <p class="mini">Your Name (required)<br />
+    {{ Form::text('name', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text wpcf7-validates-as-required',
+    'aria-required' => 'true',
+    'aria-invalid' => 'false',]) }}</p>
+
+    <p class="mini">Your Email (required)<br />
+    <span class="wpcf7-form-control-wrap your-email">{{ Form::email('email', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email',
+    'aria-required' => 'true',
+    'aria-invalid' => 'false',]) }}</span> </label></p>
+
+    <p class="mini">Subject<br />
+    <span class="wpcf7-form-control-wrap your-subject">{{ Form::text('subject', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text',
+    'aria-invalid' => 'false',]) }}</span></p>
+
+    <p class="mini">Your Message<br />
+    <span class="wpcf7-form-control-wrap your-message">{{ Form::textarea('message', null, [
+    'cols' => 40,
+    'rows'=>10,
+    'class' => 'wpcf7-form-control wpcf7-textarea',
+    'aria-invalid' => 'false',]) }}</span></p>
+
+    <p class="mini"><input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit" /></p>
+    <div class="wpcf7-response-output wpcf7-display-none"></div>
+    {{ Form::close() }}
   </div><br>
 </div>
 @endsection
