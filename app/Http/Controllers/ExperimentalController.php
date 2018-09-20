afc520e10739b2f0e765d04127320d47b6694f6f
Enea Barbetta <enea.barbetta@gmail.com>
2018-09-20 10:57:25 +0200

    Minor correction in mail text

--- app/Http/Controllers/ExperimentalController.php
+++ app/Http/Controllers/ExperimentalController.php
@@ -9,7 +9,7 @@ class ExperimentalController extends Controller
 
   public function testmail() {
     
-    Mail::raw('This is just a test email sent from Cooeracy server.', function ($message) {
+    Mail::raw('This is just a test email sent from Cooperacy server.', function ($message) {
         $message->to('cooperacy@cooperacy.org','Cooperacy')
             ->from('cooperacy@cooperacy.org','Cooperacy')
             ->subject('Test e-mail from Cooperacy');
