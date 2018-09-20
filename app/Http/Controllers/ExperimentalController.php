6a9f47a23a8755cf20d83f9cfae69a6a8c9fe86e
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-09-20 13:14:01 +0200

    minors test email

--- app/Http/Controllers/ExperimentalController.php
+++ app/Http/Controllers/ExperimentalController.php
@@ -9,7 +9,7 @@ class ExperimentalController extends Controller
 
   public function testmail() {
 
-    send('emails.welcome', [], function($message) {
+    Mail::send('emails.welcome', [], function($message) {
         $message->to('cooperacy@cooperacy.org','Cooperacy')
             ->from('cooperacy@cooperacy.org','Cooperacy')
             ->subject('Test e-mail from Cooperacy');
