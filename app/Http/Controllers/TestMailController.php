6091202470593168244416c4c757f7b4312eb1ee
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-10-06 17:26:25 +0200

    minors

--- app/Http/Controllers/TestMailController.php
+++ app/Http/Controllers/TestMailController.php
@@ -1,24 +1,31 @@
 <?php
 namespace App\Http\Controllers;
 use Mail;
-use App\Http\Controllers\Controller;
+
 
 class TestMailController extends Controller
 {
-  public function testmail () {
-        Mail::send('emails.welcome', [], function($message) {
-            $message->to('cooperacy@cooperacy.org')
-            ->from('cooperacy@cooperacy.org', 'Cooperacy')
-            ->subject('Welcome to Cooperacy');
-        });
+  public function testmail() {
 
-  public function testmailpaid() {
-    Mail::send('emails.welcome-paid', [], function($message) {
+    Mail::send('emails.welcome', [], function($message) {
         $message->to('cooperacy@cooperacy.org','Cooperacy')
             ->from('cooperacy@cooperacy.org','Cooperacy')
-            ->subject('Test paid e-mail from Cooperacy');
+            ->subject('Test e-mail from Cooperacy');
     });
+
     return 'Test e-mail sent';
   }
-}
+
+    public function testmailpaid() {
+
+      Mail::send('emails.welcome-paid', [], function($message) {
+          $message->to('cooperacy@cooperacy.org','Cooperacy')
+              ->from('cooperacy@cooperacy.org','Cooperacy')
+              ->subject('Test paid e-mail from Cooperacy');
+      });
+
+      return 'Test e-mail sent';
+
+  }
+
 }
