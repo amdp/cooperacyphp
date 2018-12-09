a068e2a49e195ef141f86b38437cd9ddb4d55dbd
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2018-12-09 12:58:07 +0100

    minors on governance

--- resources/views/pages/governance.blade.php
+++ resources/views/pages/governance.blade.php
@@ -27,14 +27,18 @@
   <br><br><p class="base">The Cooperacy governance is the way <a class="ae" href="/participate">participants</a> take decisions in Cooperacy, according to cooperation science insights.</p><br /><br />
 
   <p class="subheading freedom center space">MODULES</p><br/>
-  <p class="base">The Cooperacy governance is made of modules, that can have submodules. Every module is based on the most simple form of <a class="ae" href="/cooperation">cooperation</a>: two or more people doing something together. It is strongly advised every module should have maximum 148 people and two coordinators. Anytime, coordinators may resign, can be substituted upon the module participants’ will or there could be a candidate to become coordinator. A single coordinator is considered provisory, while modules may also decide to elect everyone as coordinator.<br />
+  <p class="base">The Cooperacy governance is made of modules, that can have submodules. Every module is based on the most simple form of <a class="ae" href="/cooperation">cooperation</a>: two or more people doing something together. <br>
+  It is strongly advised every module should have maximum 148 people and two coordinators. A single coordinator is considered provisory, while modules may also decide to elect everyone as coordinator.<br>
+  Anytime, coordinators may resign, can be substituted upon the module participants’ will or there could be a candidate to become coordinator.<br />
   </p><br/>
 
   <p class="subheading freedom center space">DECISIONS</p><br/>
-  <p class="base">Coordinators may take direct decisions in their module. Only when their decision is publicly communicated it becomes effective. When they are in contrast or unsure about what decision to take, coordinators can ask advice to the participants or start a voting phase.<br>
+  <p class="base">Coordinators may take direct decisions in their module. Only when their decision is publicly communicated it becomes effective. When they are in contrast or unsure about what decision to take, coordinators can ask advice to the participants or start a voting phase.<br><br>
+
   Participants can propose decisions as well through voting or asking the coordinators for direct approval. The coordinators should abstain when they are the voting main subject.<br>
-  Participants may also oppose the coordinators decisions. If the opposing participants are at least two<SUP>1</SUP>, the coordinators should proportionally and creatively <a class="ad" href="/integration">integrate</a> the opposition according to the seven <a class="ae" href="/integration#solutions">solutions</a> or alternatively start a voting phase.<br>
-  If a single participants proposes or opposes, but not seconded, their instance is recorded or, when possible, proportionally and creatively <a class="ad" href="/integration">integrated</a>.<br>
+  Participants may also oppose the coordinators decisions. If the opposing participants are at least two<SUP>1</SUP>, the coordinators should proportionally and creatively <a class="ad" href="/integration">integrate</a> the opposition according to the seven <a class="ae" href="/integration#solutions">solutions</a> or alternatively start a voting phase.<br><br>
+
+  If just a single participants proposes or opposes, but nobody secondes, the single instance is recorded or, when possible, but not always, proportionally and creatively <a class="ad" href="/integration">integrated</a>.<br>
 
   <br><img class="page-header" src="{{asset('/images/decisions.png')}}">
 
@@ -49,10 +53,10 @@
   - Retrieve more information, listen to all the people positions and decide again<br>
   - Separate the module in new ones, splitting eventual budgets<br>
   - Evaluate things through data, common values or objective criterias and vote again<br>
-  - Use proportional creative <a class="af" href="/integration">proportional creative integration</a> with the different solutions or give people or the same solutions specific roles<br></p>
+  - Combine using the <a class="af" href="/integration">proportional creative integration</a> the different solutions or give people -or the same solutions- specific roles<br></p>
   <br><br>
 
-  <p class="mini just">1: DUAL COORDINATION The idea of a coordinating pair came up in order to avoid power to be concentrated in just one person, that receives huge responsibilities and is alone in facing them, or in more than two people, that may generate internal conflictual groups or collusions. Collective coordination is also possible, although roles will emerge anyways.<br>
+  <p class="mini just">1: DUAL COORDINATION The idea of a coordinating pair came up in order to avoid power to be concentrated in just one person, that receives huge responsibilities and is alone in facing them, or in more than two people, that may generate internal conflictual groups or collusions. Collective coordination is also possible, although roles could emerge anyways.<br>
   2: VOTING IS INFORMATIVE The voting results are indicators of the participants will, they do not explicitly determine a final decision, because winning and losing the voting process is substituted by <a class="af" href="/integration">proportional creative integration</a>.</p>
 
   </div>
