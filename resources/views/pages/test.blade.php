22c548b395d47ed1c68693a815a53e81e04f4217
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-02-03 23:22:37 +0330

    update HUMAN video
    automatic start/end

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -6,7 +6,7 @@
 
 <!--BEGIN CONTAINER-->
 <div class="containerone">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/vdb4XGVTHkE?start=425&end=460” frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://v637g.app.goo.gl/mEZwVJ2nf2CHoxgm6” frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 <!--END CONTAINER-->
 
