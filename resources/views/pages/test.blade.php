cbf56c7675b848f0dc44f51ecade3e1f3a628306
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-02-07 10:31:36 +0330

    upate HUMAN video

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -5,10 +5,50 @@
 @section('content')
 
 <!--BEGIN CONTAINER-->
-<div class="containerone">
-  <iframe width="560" height="315" src="https://v637g.app.goo.gl/mEZwVJ2nf2CHoxgm6” frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<!--END CONTAINER-->
+<!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+<iframe style="margin-top: 60px;" id='player' width="560" height="315" src="https://www.youtube.com/embed/vdb4XGVTHkE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+<script>
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'vdb4XGVTHkE',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+</script>
 
 @stop
