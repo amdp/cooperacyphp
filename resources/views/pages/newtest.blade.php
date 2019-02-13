443bd1b9dd9f8387b12da4acb0c2b25626827bbc
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2019-02-13 23:01:37 +0100

    other HUMAN tests

--- resources/views/pages/newtest.blade.php
+++ resources/views/pages/newtest.blade.php
@@ -7,5 +7,7 @@
 <!--BEGIN CONTAINER-->
 <!-- 1. The <iframe> (and video player) will replace this <div> tag.  -->
 <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vdb4XGVTHkE?controls=0&amp;start=561&end=570" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vdb4XGVTHkE?start=561&end=590" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ShttAt5xtto?controls=0&amp;start=688;end=694" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 @stop
