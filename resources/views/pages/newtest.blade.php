02da53f7e6317429ce2806804bdc7a61aea68182
Alessandro Merletti de Palo <amdp@amarenapictures.com>
2019-02-13 23:23:35 +0100

    HUMAN testing

--- resources/views/pages/newtest.blade.php
+++ resources/views/pages/newtest.blade.php
@@ -6,8 +6,7 @@
 
 <!--BEGIN CONTAINER-->
 <!-- 1. The <iframe> (and video player) will replace this <div> tag.  -->
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vdb4XGVTHkE?controls=0&amp;start=561&end=570" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/vdb4XGVTHkE?start=561&end=590" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ShttAt5xtto?controls=0&amp;start=688;end=694" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+<br><br><br><br>
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bWTFwYbscnk?controls=0&amp;start=10;end=13;rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="560" height="315" src="https://www.youtube.com/embed/vdb4XGVTHkE?controls=0&amp;start=3182;end=3225;rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h2 class="diversity"> ----- the left video is for local testing, the right one is the one we want</h2>
 @stop
