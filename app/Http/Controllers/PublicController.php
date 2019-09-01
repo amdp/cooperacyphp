782060f79b709106c95e4b4156a161c8e18c112b
Enea Barbetta <enea.barbetta@gmail.com>
2019-09-01 13:00:22 +0200

    Correction

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -7,9 +7,9 @@ class PublicController extends Controller
     function membersnumber() {
         try {
             $membersnumber = DB::table('users')->where('member',1)->count() + DB::table('users')->where('admin',1)->count();
+            return view('pages.participate', compact('membersnumber'));
         } catch (\Exception $e) {
             echo $e->getMessage();
         }
-        return view('pages.participate', compact('membersnumber'));
     }
 }
